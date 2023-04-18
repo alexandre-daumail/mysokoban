b7df3c7a4a09a7690c077da8484781723ce378de
Alexandre DAUMAIL <alexandre.daumail@laplateforme.io>
2023-04-18 15:38:20 +0200

    game and player modules created

---
+++ src/modules/player/__init__.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((50, 50))
+        self.image.fill((255, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = 5
+
+    def update(self, keys):
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+        if keys[pygame.K_UP]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN]:
+            self.rect.y += self.speed
